96e3d9d13229a07a881949e65435497ef0c6ef98
Davi Machado de Oliveira <machadodavi276@gmail.com>
2026-04-30 09:26:14 -0300

    Versao 2.0

--- js/jogo.js
+++ js/jogo.js
@@ -30,8 +30,8 @@
       //percorremos todas as divs armazenadas
       for (i = 0; i < divis.length; i++) {
         //verificamos se sao as divs com ids 0 ou 1 ou 2
-        if (divis[i].id == 0 || divis[i].id == 1 || divis[i].id == 2) {
-          //alteramos a classe css das divs 0, 1 e 2 (className)
+        if (divis[i].id == 0 || divis[i].id == 1 || divis[i].id == 2 || divis[i].id == 3 || divis[i].id == 4) {
+          //alteramos a classe css das divs 0, 1 e 2 3 (className)
           divis[i].className = "inicial";
         }
       }
@@ -83,7 +83,7 @@
           btnReiniciar.className = 'visivel';
         }
         //a variável sorteado recebe um valor inteiro (Math.floor) aleatório (Math.random)
-        let sorteado = Math.floor(Math.random() * 3);
+        let sorteado = Math.floor(Math.random() * 4);
         //se o id da <div> escolhida pelo jogador for igual ao número sorteado
         if (obj.id == sorteado) {
           //chama a funçao acertou passando a div escolhida pelo jogador
